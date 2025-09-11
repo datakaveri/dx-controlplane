a096926069cae94f738544ab3e82bd69ce6e6f74
Ankit Singh <101859999+ankitmashu@users.noreply.github.com>
2025-09-11 10:29:05 +0530

    connector service

--- src/main/java/org/cdpg/dx/aaa/connector/service/ConnectorServiceImpl.java
+++ src/main/java/org/cdpg/dx/aaa/connector/service/ConnectorServiceImpl.java
@@ -12,9 +12,11 @@ import org.cdpg.dx.databroker.util.Vhosts;
 public class ConnectorServiceImpl implements ConnectorService {
   private static final Logger LOGGER = LogManager.getLogger(ConnectorServiceImpl.class);
   private final DataBrokerService dataBrokerService;
+  private final String publishEx;
 
-  public ConnectorServiceImpl(DataBrokerService dataBrokerService) {
+  public ConnectorServiceImpl(DataBrokerService dataBrokerService, String publishEx) {
     this.dataBrokerService = dataBrokerService;
+    this.publishEx = publishEx;
   }
 
   @Override
@@ -31,13 +33,19 @@ public class ConnectorServiceImpl implements ConnectorService {
             queueCreated -> {
               LOGGER.info("Queue created in DataBroker {}", queueCreated.toJson());
               return dataBrokerService.queueBinding(
-                  "publishEx", assetId, assetId, Vhosts.IUDX_INTERNAL);
+                  publishEx, assetId, assetId, Vhosts.IUDX_INTERNAL);
             })
         .compose(
             queueBinded -> {
               LOGGER.info("Queue Binding successful");
               return dataBrokerService.updatePermission(
-                  userId, "assetId/amq.default", PermissionOpType.ADD_WRITE, Vhosts.IUDX_INTERNAL);
+                  userId, assetId, PermissionOpType.ADD_READ, Vhosts.IUDX_INTERNAL);
+            })
+        .compose(
+            queueBinded -> {
+              LOGGER.info("Updated read permission for user");
+              return dataBrokerService.updatePermission(
+                  userId, "amq.default", PermissionOpType.ADD_WRITE, Vhosts.IUDX_INTERNAL);
             })
         .onSuccess(
             permissionUpdated -> {
@@ -52,31 +60,32 @@ public class ConnectorServiceImpl implements ConnectorService {
     return promise.future();
   }
 
-    @Override
-    public Future<String> deleteConnector(String userId, String assetId) {
-        Promise<String> promise = Promise.promise();
-        if (assetId == null || assetId.isEmpty() || userId == null || userId.isEmpty()) {
-            promise.fail(new DxBadRequestException("Invalid input or blank value"));
-            return promise.future();
-        }
-
-        dataBrokerService.deleteQueue(assetId, userId, Vhosts.IUDX_INTERNAL)
-            .compose(
-                queueDeleted -> {
-                    LOGGER.info("Queue deleted in DataBroker");
-                    return dataBrokerService.updatePermission(
-                        userId, "asset/amq.default", PermissionOpType.DELETE_WRITE, Vhosts.IUDX_INTERNAL);
-                })
-            .onSuccess(
-                permissionUpdated -> {
-                    LOGGER.info("Permission updated successfully");
-                    promise.complete("Connector deleted successfully");
-                })
-            .onFailure(
-                err -> {
-                    LOGGER.error("Error in deleting connector: {}", err.getMessage());
-                    promise.fail(err);
-                });
-        return promise.future();
+  @Override
+  public Future<String> deleteConnector(String userId, String assetId) {
+    Promise<String> promise = Promise.promise();
+    if (assetId == null || assetId.isEmpty() || userId == null || userId.isEmpty()) {
+      promise.fail(new DxBadRequestException("Invalid input or blank value"));
+      return promise.future();
     }
+
+    dataBrokerService
+        .deleteQueue(assetId, userId, Vhosts.IUDX_INTERNAL)
+        .compose(
+            queueDeleted -> {
+              LOGGER.info("Queue deleted in DataBroker");
+              return dataBrokerService.updatePermission(
+                  userId, assetId, PermissionOpType.DELETE_READ, Vhosts.IUDX_INTERNAL);
+            })
+        .onSuccess(
+            permissionUpdated -> {
+              LOGGER.info("Permission updated successfully");
+              promise.complete("Connector deleted successfully");
+            })
+        .onFailure(
+            err -> {
+              LOGGER.error("Error in deleting connector: {}", err.getMessage());
+              promise.fail(err);
+            });
+    return promise.future();
+  }
 }
