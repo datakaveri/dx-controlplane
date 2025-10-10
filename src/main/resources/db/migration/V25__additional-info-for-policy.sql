da01aa32d9c62aa569e97f98f987385a7f4a1295
Kranthi-Guribilli <kranthiguribilli@gmail.com>
2025-10-10 13:10:52 +0530

    chore(db): make acl policy schema migrations idempotent for reruns

--- src/main/resources/db/migration/V25__additional-info-for-policy.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE policy ADD COLUMN additional_info JSONB;
-ALTER TABLE policy ADD COLUMN provider_comment varchar(4000);
-ALTER TABLE policy ADD COLUMN feedback_to_consumer varchar(4000);
